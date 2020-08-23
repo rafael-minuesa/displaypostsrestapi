e29fa4189947e6dd1290cac5dad2c6c009a523a2
Rafael Minuesa <rafael.minuesa@gmail.com>
2020-08-23 07:25:39 +0200

    Update displaypostsrestapi.php

--- displaypostsrestapi.php
+++ displaypostsrestapi.php
@@ -21,44 +21,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Get posts via REST API.
  */
-function display_protonmail_posts() {
+ function display_protonmail_posts() {
 
 	// Initialize variable.
-	$allposts = '';
-	
-	// End point of ProtonMail Blog and filter the result to limit the display to 5 posts.
-	$response = wp_remote_get( 'https://protonmail.com/blog/wp-json/wp/v2/posts?per_page=5' );
+    $allposts = '';
+    
+    // End point of ProtonMail Blog and filter the result to limit the display to 5 posts.
+    $response = wp_remote_get( add_query_arg( array(
+    'per_page' => 5,
+     ), 'https://protonmail.com/blog/wp-json/wp/v2/posts?_embed' ) 
+);
 
-	// Exit if error.
-	if ( is_wp_error( $response ) ) {
-		return;
-	}
+if( !is_wp_error( $response ) && $response['response']['code'] == 200 ) {
 
-	// Get the body.
-	$posts = json_decode( wp_remote_retrieve_body( $response ) );
+   $remote_posts = json_decode( $response['body'] ); 
+   foreach( $remote_posts as $remote_post ) {
+    $thumb_full_url = '';
+    $thumb_url = '';
 
-	// Exit if nothing is returned.
-	if ( empty( $posts ) ) {
-		return;
-	}
+    // Featured image URL can be retrieved like this:
+    if ( ! empty( $remote_post->featured_media ) && isset( $remote_post->_embedded ) ) {
+        $thumb_full_url = $remote_post->_embedded->{'wp:featuredmedia'}[0]->source_url;
+        $thumb_url = $remote_post->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->medium->source_url;
+    }
 
-	// If there are posts.
-	if ( ! empty( $posts ) ) {
-
-		// For each post.
-		foreach ( $posts as $post ) {
-
-			// Use print_r($post); to get the details of the post and all available fields
-			// Format the date.
-			$fordate = date( 'n/j/Y', strtotime( $post->modified ) );
-
-			// Show a linked title and post date.
-			$allposts .= '<a href="' . esc_url( $post->link ) . '" target=\"_blank\">' . esc_html( $post->title->rendered ) . '</a><br />  ' . esc_html( $fordate ) . '<br />  ' . esc_html( $post->excerpt->rendered ) . '<br /><br />';
+    // Get the URL of a specific thumbnail size:
+    echo '<a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><h3>' . esc_html( $remote_post->title->rendered ) . '</h3></a>  ' .
+        '<p><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><img src="' . $thumb_url . '" /></a>' .$remote_post->excerpt->rendered . '</p>' .
+    }
 		}
 		
 		return $allposts;
 	}
 
-}
 // Register shortcode to be used on the site.
 add_shortcode( 'five_protonmail_posts', 'display_protonmail_posts' );
