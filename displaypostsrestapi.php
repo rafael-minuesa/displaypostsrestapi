4b611280ad228d6e0a3c36403dad6cbab87437e5
Rafael Minuesa <rafael.minuesa@gmail.com>
2020-08-23 14:03:34 +0200

    Update displaypostsrestapi.php
    Added Author

--- displaypostsrestapi.php
+++ displaypostsrestapi.php
@@ -13,13 +13,13 @@
  * @package displaypostsrestapi
  */
 
-// Disable direct file access.
+// Disable direct file access
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /**
- * Get posts via REST API.
+ * Get posts via REST API
  */
  function display_protonmail_posts() {
 
@@ -36,23 +36,28 @@ if( !is_wp_error( $response ) && $response['response']['code'] == 200 ) {
 
    $remote_posts = json_decode( $response['body'] ); 
    foreach( $remote_posts as $remote_post ) {
-    $thumb_full_url = '';
-    $thumb_url = '';
 
-    // Featured image URL can be retrieved like this:
+    // Format the date.
+	$fordate = date( 'j/n/Y', strtotime( $post->modified ) );
+
+    // Get Feaured Image 
     if ( ! empty( $remote_post->featured_media ) && isset( $remote_post->_embedded ) ) {
-        $thumb_full_url = $remote_post->_embedded->{'wp:featuredmedia'}[0]->source_url;
         $thumb_url = $remote_post->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->medium->source_url;
     }
-
-    // Get the URL of a specific thumbnail size:
-    echo '<a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><h3>' . esc_html( $remote_post->title->rendered ) . '</h3></a>  ' .
-        '<p><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><img src="' . $thumb_url . '" /></a>' .$remote_post->excerpt->rendered . '</p>' .
+    // Get Author Name 
+    if ( ! empty( $remote_post->author ) && isset( $remote_post->_embedded ) ) {
+        $author_name = $remote_post->_embedded->author[0]->name;
     }
+    echo '<a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><h3>' . esc_html( $remote_post->title->rendered ) . '</h3></a>  ' .
+        '<p><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><img src="' . $thumb_url . '" /></a>'   . esc_html( $fordate ) . ' by ' . esc_html( $author_name ) . '<br />  ' .$remote_post->excerpt->rendered . '</p>';
+}
 		}
 		
 		return $allposts;
 	}
 
-// Register shortcode to be used on the site.
+// Register shortcode to be used on the site
 add_shortcode( 'five_protonmail_posts', 'display_protonmail_posts' );
+
+// https://stackoverflow.com/questions/51889908/how-to-use-wpfeaturedmedia-in-wp-rest-api-response/51890483
+// https://stackoverflow.com/questions/36144270/wp-rest-api-vs-how-can-i-get-author-information-from-v2-the-author-id/40599471
