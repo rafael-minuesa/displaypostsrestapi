774a7f617a9cb993182ff3dc00526e5eab8cdf67
Rafael Minuesa <rafael.minuesa@gmail.com>
2020-08-24 11:49:58 +0200

    Update displaypostsrestapi.php
    Cosmetic changes mostly

--- displaypostsrestapi.php
+++ displaypostsrestapi.php
@@ -5,7 +5,7 @@
  * Plugin URI:   https://proton.prowoos.com/
  * Author:       Rafael Minuesa
  * Author URI:   https://www.linkedin.com/in/rafaelminuesa/
- * Version:      1.1
+ * Version:      1.2
  * Text Domain:  displaypostsrestapi
  * License:      GPL v2 or later
  * License URI:  https://www.gnu.org/licenses/gpl-2.0.txt
@@ -23,10 +23,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
  function display_protonmail_posts() {
 
-	// Initialize variable.
+	// All Posts variable
     $allposts = '';
     
-    // End point of ProtonMail Blog and filter the result to limit the display to 5 posts.
+    // End point of ProtonMail Blog and filter the result to limit the display to 5 posts
     $response = wp_remote_get( add_query_arg( array(
     'per_page' => 5,
      ), 'https://protonmail.com/blog/wp-json/wp/v2/posts?_embed' ) 
@@ -49,12 +49,13 @@ if( !is_wp_error( $response ) && $response['response']['code'] == 200 ) {
         $author_name = $remote_post->_embedded->author[0]->name;
         $author_name_url = $remote_post->_embedded->author[0]->source_url;
     }
-    echo '<a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><h3>' . esc_html( $remote_post->title->rendered ) . '</h3></a>'. esc_html( $fordate ) . ' by <a href="' . esc_url( $remote_post->$author_name_url ) . '" target=\"_blank\">' . esc_html( $author_name ) . '</a><p><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><img src="' . $thumb_url . '" /></a>'  .$remote_post->excerpt->rendered . '</p>';
+    echo '<div class="wrapper">    <div class="proton-posts-wrapper"><article class="proton-posts"><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><h3>' . esc_html( $remote_post->title->rendered ) . '</h3></a><small>'. esc_html( $fordate ) . ', by <a href="' . esc_url( $remote_post->$author_name_url ) . '" target=\"_blank\">' . esc_html( $author_name ) . '</small></a>
+    <p style="margin: 1em 0;"><a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><img src="' . $thumb_url . '" /></a>'  .$remote_post->excerpt->rendered . '<a href="' . esc_url( $remote_post->link ) . '" target=\"_blank\"><b>Read more ...</b></a></p></article></div></div>';
 }
-		}
-		
-		return $allposts;
 	}
+		
+	return $allposts;
+}
 
 // Register shortcode to be used on the site
 add_shortcode( 'five_protonmail_posts', 'display_protonmail_posts' );
